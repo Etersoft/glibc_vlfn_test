d77da931cb41c6830d992371f32e1ddd51945c07
Denis Obrezkov <reprofy@etersoft.ru>
2013-10-28 19:25:07 +0400

    Added counting of errors

--- glibc_vlfn_test/glibc_vlfn_test.c
+++ glibc_vlfn_test/glibc_vlfn_test.c
@@ -9,96 +9,120 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void open_test(char* arg) {
+int open_test(char* arg) {
 	int fd;
-//	printf("open_test: open() testing, trying to open %s:\n", arg);
 	if ((fd = open(arg,  O_RDONLY, 0)) < 0) {
 		perror("open_test: open() error");
+		return -1;
 	} else	{
 		printf("open_test: working\n");
 		close(fd);
+		return 0;
 		}
 
 }
 
-void creat_test(char* arg) {
+int creat_test(char* arg) {
 	int fd;
 	if ((fd = creat(arg,  0)) < 0) {
 		perror("crear_test: creat() error");
-	} else
+		return -1;
+	} else {
 		printf("creat_test: working\n");
+		return 0;
+		}
 }
 
-void fopen_test(char* arg) {
+int fopen_test(char* arg) {
 	FILE* fp;
 	if ((fp = fopen(arg, "r")) == NULL) {
 		perror("fopen_test: fopen() error");
+		return -1;
 	} else	{
 		printf("fopen_test: working\n");
+		return 0;
 		fclose(fp);
 		}
 }
 
 
-void stat_test(char* arg) {
+int stat_test(char* arg) {
 	struct stat stbuf;
 	if ((stat(arg, &stbuf)) == -1) {
 		perror("stat_test: stat() error");
-	} else
+		return -1;
+	} else	{
 		printf("stat_test: working \n");
+		return 0;
+		}
 }
 
 
-void unlink_test(char* arg) {
+int unlink_test(char* arg) {
 	int ret;
 	if ((ret = unlink(arg)) < 0) {
 		perror("unlink_test: unlink() error");
-	} else
+		return -1;
+	} else	{
 		printf("unlink_test: working \n");
+		return 0;
+		}
 }
 
-void remove_test(char* arg) {
+int remove_test(char* arg) {
 	int ret;
 	if ((ret = remove(arg)) < 0) {
 		perror("remove_test: remove() error");
-	} else
+		return -1;
+	} else	{
 		printf("remove_test: working \n");
+		return 0;
+		}
 }
 
-void access_test(char* arg) {
+int access_test(char* arg) {
 	int ret;
 	if ((ret = access(arg, F_OK)) < 0) {
 		perror("access_test: access() error");
-	} else
+		return -1;
+	} else	{
 		printf("access_test: working\n");
+		return 0;
+		}
 }
 
-void chmod_test(char* arg) {
+int chmod_test(char* arg) {
 	int ret;
 	if ((ret = chmod(arg, S_IRWXU)) < 0) {
 		perror("chmod_test: chmod() error");
-	} else
+		return -1;
+	} else	{
 		printf("chmod_test: working\n");
+		return 0;
+		}
 }
 
-void truncate_test(char* arg) {
+int truncate_test(char* arg) {
 	int ret;
 	if ((ret = truncate(arg, 100000)) < 0) {
 		perror("truncate_test: truncate() error");
-	} else
+		return -1;
+	} else	{
 		printf("truncate_test: working\n");
+		return 0;
+		}
 }
 
 
 
 
-void read_open_dir_test(char* arg ) {
+int read_open_dir_test(char* arg ) {
 	DIR *dir;		/*analogue FILE*/
 		
 	if ((dir=opendir("./"))==0)
 	    {
 	        perror("Diropen error\n");
-	        exit(1);
+	        return -1;
 	    };
 	    
 	/* Prosmotr failov vnutri direktorii, esli sozdan file - vyvod dannyh o nem na ekran*/
@@ -114,50 +138,68 @@ void read_open_dir_test(char* arg ) {
 		    exsts++;
 		};
 	};
-	if (!exsts) printf("read_open_dir_test: File isn't found\n");
+	if (!exsts)	{  
+		printf("read_open_dir_test: File isn't found\n"); 
+		return -1;
+		} else return 0;
 }
 
-void link_test(char* arg1, char* arg2) {
+int link_test(char* arg1, char* arg2) {
 	int ret;
 	if ((ret = link(arg1, arg2)) < 0) {
 		perror("link_test: link() error");
-	} else
+		return -1;
+	} else	{
 		printf("link_test: working\n");
+		return 0;
+		}
 }
 
 
-void rename_test(char* arg1, char* arg2) {
+int rename_test(char* arg1, char* arg2) {
 	int ret;
 	if ((ret = rename(arg1, arg2)) < 0) {
 		perror("rename_test: rename() error");
-	} else
+		return -1;
+	} else	{
 		printf("rename_test: working\n");
+		return 0;
+		}
 }
 
 
-void symlink_test(char* arg1, char* arg2) {
+int symlink_test(char* arg1, char* arg2) {
 	int ret;
 	if ((ret = symlink(arg1, arg2)) < 0) {
 		perror("symlink_test: symlink() error");
-	} else
+		return -1;
+	} else	{
 		printf("symlink_test: working\n");
+		return 0;
+		}
 }
 
 
-void mkdir_test(char* arg) {
+int mkdir_test(char* arg) {
 	int ret;
 	if ((ret = mkdir(arg, S_IRUSR)) < 0) {
 		perror("mkdir_test: mkdir() error");
-	} else
+		return -1;
+	} else	{
 		printf("mkdir_test: working\n");
+		return 0;
+		}
 }
 
-void rmdir_test(char* arg) {
+int rmdir_test(char* arg) {
 	int ret;
 	if ((ret = rmdir(arg)) < 0) {
 		perror("rmdir_test: rmdir() error");
-	} else
+		return -1;
+	} else	{
 		printf("rmdir_test: working\n");
+		return 0;
+		}
 }
 
 
@@ -172,29 +214,30 @@ int main (int argc, char* argv[]) {
 	else
 		filename = "ещеболеедлинноеоченьдлинноеимяоченьдлинноеимяоченьдлинноеимяоченьдлинноеимяоченьдлинноеимя\
 оченьдлинноеимяоченьдлинноеимяоченьдлинноеимяоченьдлинноеимя";
-		
-	creat_test(filename);
-	access_test(filename);
-	chmod_test(filename);
-	open_test(filename);
-	stat_test(filename);
-	read_open_dir_test(filename);
+	int err = 0;
+	err += creat_test(filename);
+	err += access_test(filename);
+	err += chmod_test(filename);
+	err += open_test(filename);
+	err += stat_test(filename);
+	err += read_open_dir_test(filename);
 
-	unlink_test(filename);
-	creat_test(filename);
-	symlink_test(filename, "simply_symlink");
-	unlink("simply_symlink");
-	rename(filename, "newname");
-	rename("newname", filename);
-	chmod_test(filename);
-	fopen_test(filename);
-	truncate_test(filename);
-	link_test(filename, "simply_link");
-	unlink("simply_link");
-	remove_test(filename);
-	mkdir_test(filename);
-	rmdir_test(filename);
+	err += unlink_test(filename);
+	err += creat_test(filename);
+	err += symlink_test(filename, "simply_symlink");
+	err += unlink("simply_symlink");
+	err += rename(filename, "newname");
+	err += rename("newname", filename);
+	err += chmod_test(filename);
+	err += fopen_test(filename);
+	err += truncate_test(filename);
+	err += link_test(filename, "simply_link");
+	err += unlink("simply_link");
+	err += remove_test(filename);
+	err += mkdir_test(filename);
+	err += rmdir_test(filename);
+	
+	printf("Errors: %d\n", (-err));
 	
 	return 0;
 }
-	
